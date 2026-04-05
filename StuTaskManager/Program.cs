915cbc2959454abf1052ea4a5916492db7cc6011
PastaDev63 <emman.deluna63@gmail.com>
2026-04-05 17:36:36 +0800

    Added a Constructor in Tasks class.

---
+++ StuTaskManager/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class Student
+{
+    string name = "Emmanuel de Luna";
+    double gwa = 1.28;
+    int IDNum = 202511873;
+
+    public static void Name()
+    {
+        Student myName = new Student();
+        Console.WriteLine($"Student Name: {myName.name}");
+    }
+    public static void GWA()
+    {
+        Student myGWA = new Student();
+        Console.WriteLine($"Student GWA: {myGWA.gwa}");
+    }
+    public static void ID()
+    {
+        Student myID = new Student();
+        Console.WriteLine($"Student ID: {myID.IDNum}");
+    }
+}
+
+
+
+public class LandingPage
+{
+    public static void Main(string[] args)
+    {
+        
+        Console.WriteLine("Welcome to the Student Task Manager!\nSign in to view your student information.");
+        Console.ReadKey();
+        Console.WriteLine("Do you want to sign in to view your student information? (Yes/No)");
+        string answer = Console.ReadLine();
+
+        if (answer == "Yes")
+        {
+            Console.WriteLine("Signing in...");
+        }
+
+        Console.ReadKey();
+    }
+}
+
